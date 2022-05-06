e5339eac55dd50e6f801e38ac6e2da3170406395
TadeuViegas <tadeucosta@acad.ifma.edu.br>
2022-05-06 16:14:48 -0300

    first commit

---
+++ src/br/com/dio/FirstProgram.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        Cachorro cao = new Cachorro();
+
+        System.out.println("hello word!");
+        System.out.println(cao);
+    }
+}
