6cf91fe0f7381f74322cd333f715cb2584cb4c32
TadeuViegas <tadeucosta@acad.ifma.edu.br>
2022-05-06 17:08:27 -0300

    testes com duas classes

--- src/br/com/dio/FirstProgram.java
+++ src/br/com/dio/FirstProgram.java
@@ -1,12 +1,16 @@
 package br.com.dio;
 
 import br.com.dio.model.Cachorro;
+import br.com.dio.model.Livro;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        Cachorro cao = new Cachorro();
+        Cachorro cao = new Cachorro("marley","azul",12);
+        System.out.println(cao);
 
         System.out.println("hello word!");
-        System.out.println(cao);
+
+        Livro livro = new Livro("rei dos reis",300);
+        System.out.println(livro);
     }
 }
